5919a8e4e27838f206db27397e02a5c43ca8a794
Barbara Johnson <johnson.barbara@gmail.com>
2019-04-18 02:00:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,8 +26,9 @@ class Scraper
         course.schedule = post.css(".date").text
         course.description = post.css("p").text
 
-    end
-  end 
+      end
+      binding.pry 
+    end 
     
     
     
